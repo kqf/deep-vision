7f6fbe4c7aaa72a5952fa677661455552e69188d
Sasha <q.dev.null@gmail.com>
2020-08-03 13:15:27 +0200

    Add the bottleneck ResNet blocks

--- models/resnet.py
+++ models/resnet.py
@@ -61,6 +61,62 @@ class BasicBlock(torch.nn.Module):
         return x
 
 
+class Bottleneck(torch.nn.Module):
+
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=False):
+        super().__init__()
+
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1,
+                                     stride=1, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                                     stride=stride, padding=1, bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+
+        self.conv3 = torch.nn.Conv2d(
+            out_channels, self.expansion * out_channels, kernel_size=1,
+            stride=1, bias=False)
+        self.bn3 = torch.nn.BatchNorm2d(self.expansion * out_channels)
+
+        self.relu = torch.nn.ReLU(inplace=True)
+
+        if downsample:
+            conv = torch.nn.Conv2d(
+                in_channels, self.expansion * out_channels, kernel_size=1,
+                stride=stride, bias=False)
+            bn = torch.nn.BatchNorm2d(self.expansion * out_channels)
+            downsample = torch.nn.Sequential(conv, bn)
+        else:
+            downsample = None
+
+        self.downsample = downsample
+
+    def forward(self, x):
+        i = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.downsample is not None:
+            i = self.downsample(i)
+
+        x += i
+        x = self.relu(x)
+
+        return x
+
+
 class ResNet(torch.nn.Module):
     def __init__(self, input_dim, output_dim, freeze_features=False):
         super().__init__()
