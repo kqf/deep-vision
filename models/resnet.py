d53049a00d7aa06d24dcf656886d5658dbb9a421
Sasha <q.dev.null@gmail.com>
2020-08-03 10:12:18 +0200

    Add ResNet basic blocks

--- models/resnet.py
+++ models/resnet.py
@@ -16,6 +16,51 @@ torch.cuda.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 
+class BasicBlock(torch.nn.Module):
+
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=False):
+        super().__init__()
+
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                                     stride=stride, padding=1, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                                     stride=1, padding=1, bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+
+        self.relu = torch.nn.ReLU(inplace=True)
+
+        if downsample:
+            conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1,
+                                   stride=stride, bias=False)
+            bn = torch.nn.BatchNorm2d(out_channels)
+            downsample = torch.nn.Sequential(conv, bn)
+        else:
+            downsample = None
+
+        self.downsample = downsample
+
+    def forward(self, x):
+        i = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        if self.downsample is not None:
+            i = self.downsample(i)
+
+        x += i
+        x = self.relu(x)
+        return x
+
+
 class ResNet(torch.nn.Module):
     def __init__(self, input_dim, output_dim, freeze_features=False):
         super().__init__()
